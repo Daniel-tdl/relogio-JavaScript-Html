8bbd0d892f224f0c60471ef6aeda97596a2c3be0
Daniel Coutinho <daniel.coutinho.tdl@gmail.com>
2020-11-21 17:16:48 -0300

    Desenhando ponteiros relogio

--- relogio.js
+++ relogio.js
@@ -1,6 +1,7 @@
 function acionarRelogio() {
-    desenharCirculo()
-    desenharNumeros()
+    desenharCirculo();
+    desenharNumeros();
+    desenharPonteiros();
 }
 
 function desenharCirculo() {
@@ -40,4 +41,42 @@ function desenharNumeros() {
         contexto.translate(0, circulo * 0.85);
         contexto.rotate(-ang);
     }
+}
+
+function desenharPonteiros() {
+    var dataAtual = new Date();
+    var horas = dataAtual.getHours();
+    var minutos = dataAtual.getMinutes();
+    var segundos = dataAtual.getSeconds();
+
+    desenharPonteiroHora(horas, minutos, segundos);
+    desenharPonteiroMinutos(minutos, segundos);
+    desenharPonteiroSegundos(segundos);
+}
+
+function desenharPonteiroHora(horas, minutos, segundos) {
+    horas = horas % 12;
+    horas = (horas * Math.PI / 6) + (minutos * Math.PI / (6 * 60)) + (segundos * Math.PI / (360 * 60));
+    definindoPosicaoPonteiro(contexto, horas, circulo * 0.5, circulo * 0.07);
+}
+
+function desenharPonteiroMinutos(minutos, segundos) {
+    minutos = (minutos * Math.PI / 30) + (segundos * Math.PI / (30 * 60));
+    definindoPosicaoPonteiro(contexto, minutos, circulo * 0.8, circulo * 0.07);
+}
+
+function desenharPonteiroSegundos(segundos) {
+    segundos = (segundos * Math.PI / 30);
+    definindoPosicaoPonteiro(contexto, segundos, circulo * 0.9, circulo * 0.02);
+}
+
+function definindoPosicaoPonteiro(contexto, pos, length, width) {
+    contexto.beginPath();
+    contexto.lineWidth = width;
+    contexto.lineCap = "round";
+    contexto.moveTo(0, 0);
+    contexto.rotate(pos);
+    contexto.lineTo(0, -length);
+    contexto.stroke();
+    contexto.rotate(-pos);
 }
